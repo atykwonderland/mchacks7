43a6f20a7fe5f8a47bffa4e30ed85d1ef4e94919
root <root@LAPTOP-9P97HEFG.localdomain>
2020-02-01 16:18:07 -0500

    updated database connection

--- earthr/db.py
+++ earthr/db.py
@@ -4,6 +4,6 @@ from flask import jsonify
 mongo = pymongo.MongoClient("mongodb+srv://alice:alice123@cluster0-bd4ay.mongodb.net/test?retryWrites=true&w=majority", connect=False) #insert string with mongo information
 db = mongo.test
 #db = pymongo.database.Database(mongo, 'mydatabase')
-col = db.earthr3
+col = db.earthr
 data = [food for food in col.find()]
 
